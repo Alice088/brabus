1321b2d5a58a107ba4cc7c68c8886c0e995ab70d
alice088 <alien280579@gmail.com>
2024-12-28 04:59:52 +0300

    test

--- cmd/brabus.go
+++ cmd/brabus.go
@@ -2,67 +2,70 @@ package main
 
 import (
 	_ "brabus/internal/brokers"
+	"brabus/internal/metrics"
+	"brabus/internal/metrics/cpu"
+	"brabus/internal/metrics/disk"
+	"brabus/internal/metrics/ram"
+	"brabus/internal/yaml"
 	"fmt"
-	nats2 "github.com/nats-io/nats.go"
+	_ "fmt"
+	"github.com/mailru/easyjson"
+	natsPkg "github.com/nats-io/nats.go"
 	"log"
+	"time"
 )
 
 func main() {
-	nc, err := nats2.Connect(nats2.DefaultURL)
+	nc, err := natsPkg.Connect(natsPkg.DefaultURL)
+	defer nc.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = nc.Publish("foo", []byte("Hello World"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	globalConf := yaml.UnmarshalGlobalConfig()
+	failLimit := globalConf.Limits.FailLimit
+	failCount := 0
 
-	// Simple Async Subscriber
-	_, err = nc.Subscribe("foo", func(m *nats2.Msg) {
-		fmt.Printf("Received a message: %s\n", string(m.Data))
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		time.Sleep(2 * time.Second)
+		CPUUsage := cpu.GetCPUUsage()
+		RAMUsage := ram.GetRAMUsage()
+		DiskSpace := disk.GetDiskFreeSpace()
+		DiskUsage := disk.GetDiskUsage()
 
-	//globalConf := yaml.UnmarshalGlobalConfig()
-	//failLimit := globalConf.Limits.FailLimit
-	//failCount := 0
-	//
-	//for {
-	//	time.Sleep(2 * time.Second)
-	//	CPUUsage := cpu.GetCPUUsage()
-	//	RAMUsage := ram.GetRAMUsage()
-	//	DiskSpace := disk.GetDiskFreeSpace()
-	//	DiskUsage := disk.GetDiskUsage()
-	//
-	//	someStruct := &metrics.Metrics{
-	//		CPUUsage:  CPUUsage,
-	//		RAMUsage:  RAMUsage,
-	//		DiskSpace: DiskSpace,
-	//		DiskUsage: DiskUsage,
-	//	}
-	//	rawBytes, err := easyjson.Marshal(someStruct)
-	//
-	//	if err != nil {
-	//		failCount++
-	//		if failCount > failLimit {
-	//			log.Fatalf("Fatal error marshalling metrics: %v", err)
-	//		}
-	//
-	//		log.Printf("Error during marshalling metrics: %v\n", err)
-	//		continue
-	//	}
-	//
-	//	err = nats.Publish("metrics", rawBytes)
-	//	if err != nil {
-	//		failCount++
-	//		if failCount > failLimit {
-	//			log.Fatalf("Fatal error during publish metrics %v", err)
-	//		}
-	//
-	//		log.Printf("Error during publish metrics: %v\n", err)
-	//	}
-	//}
+		someStruct := &metrics.Metrics{
+			CPUUsage:  CPUUsage,
+			RAMUsage:  RAMUsage,
+			DiskSpace: DiskSpace,
+			DiskUsage: DiskUsage,
+		}
+		rawBytes, err := easyjson.Marshal(someStruct)
+
+		if err != nil {
+			failCount++
+			if failCount > failLimit {
+				log.Fatalf("Fatal error marshalling metrics: %v", err)
+			}
+
+			log.Printf("Error during marshalling metrics: %v\n", err)
+			continue
+		}
+
+		_, err = nc.Subscribe("metrics", func(msg *natsPkg.Msg) {
+			fmt.Printf("Received a message: %s\n", string(msg.Data))
+		})
+		if err != nil {
+			return
+		}
+
+		err = nc.Publish("metrics", rawBytes)
+		if err != nil {
+			failCount++
+			if failCount > failLimit {
+				log.Fatalf("Fatal error during publish metrics %v", err)
+			}
+
+			log.Printf("Error during publish metrics: %v\n", err)
+		}
+	}
 }
