6d3ac8dd828e72fed14d4b0a5cd386642b119da1
Alice088 <alien280579@gmail.com>
2025-07-06 12:24:32 +0300

    severity level for anomalies

--- internal/app/banana/analyze/cpu/anomalies.go
+++ internal/app/banana/analyze/cpu/anomalies.go
@@ -8,10 +8,10 @@ import (
 func CheckAnomalies(cpu dto.CPU, anomalies *[]string) {
 	totalUsed := cpu.Average.User + cpu.Average.System
 	if totalUsed > 90.0 {
-		*anomalies = append(*anomalies, fmt.Sprintf("High CPU usage: %.2f%%", float64(totalUsed)))
+		*anomalies = append(*anomalies, fmt.Sprintf("[warning] High CPU usage: %.2f%%", float64(totalUsed)))
 	}
 
 	if cpu.Average.IOWait > 10.0 {
-		*anomalies = append(*anomalies, fmt.Sprintf("High IOWait: %.2f%%", cpu.Average.IOWait))
+		*anomalies = append(*anomalies, fmt.Sprintf("[warning] High IOWait: %.2f%%", cpu.Average.IOWait))
 	}
 }
