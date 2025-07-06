f0103bbf82917e9ed7c1954d6b9b233e0460df84
Alice088 <alien280579@gmail.com>
2025-07-06 10:12:24 +0300

    fix banan

--- cmd/banana/banana.go
+++ cmd/banana/banana.go
@@ -3,46 +3,68 @@ package main
 import (
 	"brabus/internal/app/banana/analyze"
 	"brabus/internal/app/banana/storage/badger"
+	"brabus/pkg/config"
 	"brabus/pkg/dto"
 	"brabus/pkg/env"
 	"brabus/pkg/log"
+	"brabus/pkg/utils"
+	"github.com/avast/retry-go"
+	bad "github.com/dgraph-io/badger/v4"
 	"github.com/mailru/easyjson"
 	"github.com/nats-io/nats.go"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	env.Init()
-	log, closeLog := log.Init()
+	logger, closeLog := log.Init()
 	defer closeLog()
 
-	zerolog, closeLog := log.Init()
-	defer closeLog()
+	conf := config.Init()
+
+	if conf.Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		logger.Debug().Msgf("Config: %+v", conf)
+	}
 
 	db, err := badger.Init()
 
 	if err != nil {
-		zerolog.Fatal().Stack().Err(err).Msg("Error init badger")
+		logger.Fatal().Stack().Err(err).Msg("Error init badger")
 	}
 
-	defer db.Close()
+	defer func(db *bad.DB) {
+		err := db.Close()
+		if err != nil {
+			logger.Error().Err(err).Msg("Error closing db")
+		}
+	}(db)
 
 	metrics := dto.Metrics{}
 
-	nc, err := nats.Connect(nats.DefaultURL)
+	utils.DefaultRetryConfig = utils.DefaultRetry(conf.Limit.Fail, 1, logger)
+
+	var connect *nats.Conn
+	err = retry.Do(func() error {
+		connect, err = nats.Connect(nats.DefaultURL)
+		return err
+	}, utils.DefaultRetryConfig...)
+
 	if err != nil {
-		log.Fatal().Stack().Err(log.WrapError(err)).
+		logger.Fatal().
 			Str("NATS_URL", nats.DefaultURL).
-			Msg("Error connecting to nats server")
+			Err(err).
+			Msg("error connecting to nats server")
 	}
 
-	defer nc.Close()
+	defer connect.Close()
 
-	log.Info().Msg("Connected to NATS")
+	logger.Info().Msg("Connected to NATS")
 
 	_, err = nc.Subscribe("metrics", func(msg *nats.Msg) {
 		err = easyjson.Unmarshal(msg.Data, &metrics)
 		if err != nil {
-			log.Fatal().Stack().Err(log.WrapError(err)).
+			logger.Fatal().Stack().Err(logger.WrapError(err)).
 				Str("NATS_URL", nats.DefaultURL).
 				Msg("Error unmarshalling metrics")
 		}
@@ -51,7 +73,7 @@ func main() {
 	})
 
 	if err != nil {
-		log.Fatal().Stack().Err(log.WrapError(err)).
+		logger.Fatal().Stack().Err(logger.WrapError(err)).
 			Str("NATS_URL", nats.DefaultURL).
 			Msg("Error subscribing to NATS")
 	}
