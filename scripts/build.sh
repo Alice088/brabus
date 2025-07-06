f617205f7f4d60f361945c5cc627b98148eef81f
Alice088 <alien280579@gmail.com>
2025-07-06 12:58:27 +0300

    prod

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ export PATH=$PATH:/home/gosha/go/bin
 
 # shellcheck disable=SC2164
 cd ../cmd/brabus
-go build -o ../../prod/bin/brabus
+go build -o ../../bin/brabus
 
 # shellcheck disable=SC2164
 cd ../banana
-go build -o ../../prod/bin/banana
+go build -o ../../bin/banana
